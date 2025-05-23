2bf12ffb910ea950ce925db49352ca28203d9b3f
Ilya <ilya2008n@gmail.com>
2025-05-23 15:10:03 +0300

    не помню что сделал, скорее всего правки опять

--- console_game/Enemy.cs
+++ console_game/Enemy.cs
@@ -1,13 +1,21 @@
 using ConsoleGameEngine;
 namespace console_game;
 
-public class Enemy
+public class Enemy(Point pos)
 {
-    public Point pos;
+    public Point Pos = pos;
 
-    public Enemy(Point pos)
-    {
-        this.pos = pos;
+    public void Attack(Player player, Point offset)
+    {   
+        var playerPos = player.Pos + offset;
+        int dist = Convert.ToInt32(Math.Ceiling(Point.Distance(player.Pos, Pos)));
+        if (dist > 1)
+        {
+            if (Pos.X > playerPos.X) { Pos.X -= 1; }
+            else if (Pos.X < playerPos.X) { Pos.X += 1; }
+            if (Pos.Y > playerPos.Y) { Pos.Y -= 1; }
+            else if (Pos.Y < playerPos.Y) { Pos.Y += 1; }
+        }
+        else if (dist <= 1) { player.Health -= 1; }
     }
-
 }
--- console_game/Program.cs
+++ console_game/Program.cs
@@ -1,20 +1,19 @@
 ﻿using ConsoleGameEngine;
 namespace console_game;
 
-
-
 internal class Program: ConsoleGame
 {
     private static void Main(string[] args) {
         new Program().Construct(16, 17, 16, 16, FramerateMode.MaxFps);
-        Console.Title = "Console Game";
     }
 
     private int[,]? _map; //TODO класс Map для карты
     private Player _player = new(0);
     public List<Point> PlayerAtck  = new();
     public List<Enemy> Enemies = new();
+    readonly Point _offset = new Point(1, 2);
     public override void Create() {
+        Console.Title = "Console Game";
         Engine.SetPalette(Palettes.Default);
         //Engine.Borderless();
         // this.DeltaTime = 1 / 30;
@@ -64,17 +63,18 @@ internal class Program: ConsoleGame
             try { Enemies.Add(new Enemy(pos)); }
             catch (Exception e) { }
         }
+        
+        Enemies.ForEach(Enemy => Enemy.Attack(_player, _offset));
     }
 
     public override void Render() {
         Engine.ClearBuffer();
         Engine.Frame(new Point(0, 1), new Point(15, 16), 7);
-        Point offset = new Point(1, 2);
 
         for (var y = 0; y < _map.GetLength(0); y++)
         for (var x = 0; x < _map.GetLength(1); x++)
         {
-            var p = new Point(x, y) + offset;
+            var p = new Point(x, y) + _offset;
 
             switch (_map[y, x])
             {
@@ -91,28 +91,23 @@ internal class Program: ConsoleGame
         }
 
         Enemies.ForEach(enemy => { 
-            try { Engine.SetPixel(enemy.pos, (int)ConsoleColor.DarkRed, (ConsoleCharacter)'@'); }
+            try { Engine.SetPixel(enemy.Pos, (int)ConsoleColor.DarkRed, (ConsoleCharacter)'@'); }
             catch (Exception e) { } 
         });
         PlayerAtck.ForEach((point) => {
-            try { Engine.SetPixel(point+offset, (int)ConsoleColor.DarkCyan, ConsoleCharacter.Medium); }
+            try { Engine.SetPixel(point+_offset, (int)ConsoleColor.DarkCyan, ConsoleCharacter.Medium); }
             catch (Exception e) { }
         });
         Enemies.RemoveAll(enemy => {
-            return PlayerAtck.Any((p) =>  p.X == (enemy.pos-offset).X && p.Y == (enemy.pos-offset).Y );
+            return PlayerAtck.Any((p) =>  p.X == (enemy.Pos-_offset).X && p.Y == (enemy.Pos-_offset).Y );
         });
         PlayerAtck.Clear();
 
 
-
-
-
-
-
-
         Engine.WriteText(new Point(0,0), $"{Math.Round(this.GetFramerate())}",2 );
-        Engine.WriteText(new Point(5,0), $"{Engine.GetMousePos()}",2 );
-        Engine.SetPixel(_player.Pos + offset, _map[_player.Pos.Y, _player.Pos.X] == 4 ? 11: 8, (ConsoleCharacter)'@');
+        Engine.WriteText(new Point(3,0), $"{Engine.GetMousePos()}",2 );
+        Engine.WriteText(new Point(12,0), $"{_player.Health}",2 );
+        Engine.SetPixel(_player.Pos + _offset, _map[_player.Pos.Y, _player.Pos.X] == 4 ? 11: 8, (ConsoleCharacter)'@');
         
         Engine.DisplayBuffer();
     }
