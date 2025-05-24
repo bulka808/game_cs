852cba0edcbdd48625e4e00b06c5567d318dc4a6
Ilya <ilya2008n@gmail.com>
2025-05-24 19:07:35 +0300

    добавлен класс пули который несет в себе список точек

---
+++ console_game/Bullet.cs
@@ -0,0 +1,39 @@
+using ConsoleGameEngine;
+namespace console_game;
+
+public class Bullet
+{
+    public List<Point> Points;
+
+    public Bullet(Point startPoint, Point endPoint)
+    {
+        Points = GenerateTrajectory(startPoint, endPoint);
+    }
+
+    private List<Point> GenerateTrajectory(Point sp, Point ep)
+    {
+        List<Point> points = [];
+        var x0 = sp.X; var y0 = sp.Y;
+        var x1 = ep.X; var y1 = ep.Y;
+        
+        var sx = x0 < x1 ? 1 : -1;
+        var sy = y0 < y1 ? 1 : -1;
+        
+        var dx = Math.Abs(x1 - x0);
+        var dy = Math.Abs(y1 - y0);
+        
+        var err = dx - dy;
+
+        while (true)
+        {
+            points.Add(new Point(x0, y0));
+            if (x0 == x1 && y0 == y1) { break; }
+            var e2 = 2 * err;
+            
+            if (e2 > -dy) { err -= dy; x0 += sx; }
+            if (e2 < dx) { err += dx; y0 += sy; }
+        }
+        
+        return points;
+    }
+}
